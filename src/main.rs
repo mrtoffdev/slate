7fbbd705aaab94869b7ca9ea4a8d17d0ea878d87
mrtoffdev <pub.mrtoff@proton.me>
2023-09-06 16:09:31 +0800

    Feat: Prefix parser for section identification

--- src/main.rs
+++ src/main.rs
@@ -47,4 +47,57 @@ use time::Time;
 
 fn main() {
     println!("Hello, world!");
+
+        const SCHEDULE: &str = "./sch.in";
+        fn open_file<P>(file: P) -> io::Result<io::Lines<io::BufReader<File>>>
+        where P: AsRef<Path>, {
+                Ok(io::BufReader::new(File::open(file)?).lines())
+
+        }
+
+        // Build Schedule Node
+        let mut subject_collection: Vec<Subject> = vec![];
+        if let Ok(sch_in) = open_file(SCHEDULE) {
+                let mut SECTION = false;
+                let mut subject_holder  = Subject {
+                        name: "".to_string(),
+                        prof: "".to_string(),
+                        schedules: vec![],
+                };
+
+                for (i, line) in sch_in.into_iter().flatten().enumerate() {
+
+                        let top = subject_collection.len();
+                        // println!("{i}: {line}");
+                        if line.starts_with('+') {
+                                SECTION = true;
+
+                                if let Some(name) = line.strip_prefix('+') {
+                                        subject_holder.name = name.to_owned();
+                                }
+                                continue;
+                        }
+
+                        if line.starts_with('-') {
+                                SECTION = false;
+                                subject_collection.push(subject_holder.clone());
+                                println!("Subject Dispatched: {}",
+                                        subject_collection[top].name);
+                                // reset holder
+                                subject_holder = Subject::default();
+                        }
+
+                        if line.starts_with(' ') || line.starts_with('\n') || line
+                                .is_empty(){
+                                println!("whitespace found");
+                                continue
+                        }
+                        println!("\n")
+                }
+        }
+
+
+        println!("First Subject: {}", subject_collection[0].name);
+
+        println!("Schedule Management:");
 }
